07bbbc38c1a47bc87f34f055305bac4121afcdc3
jooda00 <dragon2000a@gmail.com>
2024-02-23 19:05:46 +0900

    refactor: Math.random() 1~6 반환하게 변경

--- src/main/java/com/inflearn/Inflearn/Study/day05/controller/DiceController.java
+++ src/main/java/com/inflearn/Inflearn/Study/day05/controller/DiceController.java
@@ -1,6 +1,7 @@
 package com.inflearn.Inflearn.Study.day05.controller;
 
 import com.inflearn.Inflearn.Study.day05.view.InputView;
+import com.inflearn.Inflearn.Study.day05.view.OutputView;
 
 import java.io.IOException;
 
@@ -12,10 +13,11 @@ public class DiceController {
             int num = extractNumber();
             dice[num]++;
         }
+        OutputView.printOutput(dice);
     }
 
     public int extractNumber() {
-        int num = (int) (Math.random() * 6);
+        int num = (int) (Math.random() * 6) + 1;
         return num;
     }
 }
