280f943fbaf70c9bf85f9cbcafa35a42803679d8
jooda00 <dragon2000a@gmail.com>
2024-02-23 20:11:02 +0900

    style: 메세지 "은" -> "은/는"으로 변경

--- src/main/java/com/inflearn/Inflearn/Study/day05/view/OutputView.java
+++ src/main/java/com/inflearn/Inflearn/Study/day05/view/OutputView.java
@@ -4,7 +4,7 @@ public class OutputView {
     public static void printOutput(int[] dice) {
         int len = dice.length;
         for(int i = 1; i <= len - 1; i++) {
-            System.out.println(i + "은 " + dice[i] + "번 나왔습니다.");
+            System.out.println(i + "은(는) " + dice[i] + "번 나왔습니다.");
         }
     }
 }
