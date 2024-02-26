32c357f4e9c247cd8098e7d3eec957a11f75f66a
jooda00 <dragon2000a@gmail.com>
2024-02-26 21:50:15 +0900

    feat: FruitMemoryRepository, FruitMySqlRepository로 분리

---
+++ src/main/java/com/inflearn/Inflearn/Study/day06/repository/FruitMemoryRepository.java
@@ -0,0 +1,53 @@
+package com.inflearn.Inflearn.Study.day06.repository;
+
+import com.inflearn.Inflearn.Study.day06.dto.FruitSoldResponse;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+public class FruitMemoryRepository implements FruitRepository{
+    private final JdbcTemplate jdbcTemplate;
+
+    public FruitMemoryRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public void save(String name, LocalDate warehousingDate, Long price) {
+        String sql = "insert into fruit(name, warehousingDate, price) values (?, ?, ?)";
+        jdbcTemplate.update(sql, name, warehousingDate, price);
+    }
+
+    public void update(Long id) {
+        String sql = "update fruit set is_sold = 1 where id = ?";
+        jdbcTemplate.update(sql, id);
+    }
+
+    public FruitSoldResponse getList(String name) {
+        String sql = "select is_sold, sum(price) from fruit where name = ? group by is_sold";
+        Map<Boolean, Long> soldStatusMap = jdbcTemplate.query(sql, new Object[]{name}, rs -> {
+            Map<Boolean, Long> map = new HashMap<>();
+            while(rs.next()) {
+                map.put(rs.getBoolean(1), rs.getLong(2));
+            }
+            return map;
+        });
+
+        Long soldPrice = soldStatusMap.getOrDefault(true, 0L);
+        Long notSoldPrice = soldStatusMap.getOrDefault(false, 0L);
+        return new FruitSoldResponse(soldPrice, notSoldPrice);
+    }
+
+    public boolean isFruitExistById(long id) {
+        String sql = "select * from fruit where id = ?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> 0, id).isEmpty();
+    }
+
+    public boolean isFruitExistByName(String name) {
+        String sql = "select * from fruit where name = ?";
+        return jdbcTemplate.query(sql, (rs, rowNum) -> 0, name).isEmpty();
+    }
+}
--- src/main/java/com/inflearn/Inflearn/Study/day06/repository/FruitRepository.java
+++ src/main/java/com/inflearn/Inflearn/Study/day06/repository/FruitRepository.java
@@ -1,11 +1,9 @@
 package com.inflearn.Inflearn.Study.day06.repository;
 
 import com.inflearn.Inflearn.Study.day06.dto.FruitSoldResponse;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
-@Repository
 public interface FruitRepository {
     void save(String name, LocalDate warehousingDate, Long price);
     void update(Long id);
