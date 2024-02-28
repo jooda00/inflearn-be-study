417976adcc3c14721ac2697b1fcff8fb80c02de3
jooda00 <dragon2000a@gmail.com>
2024-02-28 23:04:46 +0900

    refactor: findAllByPriceIsNotSold 동적 쿼리 생성

--- src/main/java/com/inflearn/Inflearn/Study/day07/repository/FruitRepositoryUsingQuerydsl.java
+++ src/main/java/com/inflearn/Inflearn/Study/day07/repository/FruitRepositoryUsingQuerydsl.java
@@ -1,8 +1,8 @@
 package com.inflearn.Inflearn.Study.day07.repository;
 
 import com.inflearn.Inflearn.Study.day07.dto.FruitResponse;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,31 +13,25 @@ import static com.inflearn.Inflearn.Study.day07.entity.QFruit.fruit;
 @Repository
 public class FruitRepositoryUsingQuerydsl {
 
-    private final EntityManager em;
     private final JPAQueryFactory queryFactory;
 
-    public FruitRepositoryUsingQuerydsl(EntityManager em, JPAQueryFactory queryFactory) {
-        this.em = em;
+    public FruitRepositoryUsingQuerydsl(JPAQueryFactory queryFactory) {
         this.queryFactory = queryFactory;
     }
 
     public List<FruitResponse> findAllByPriceIsNotSold(String opt, Long price) {
-        if(opt.equals("GTE")) {
-            return queryFactory
-                    .selectFrom(fruit)
-                    .where(fruit.price.goe(price), fruit.isSold.eq(false))
-                    .fetch()
-                    .stream()
-                    .map(FruitResponse::new)
-                    .collect(Collectors.toList());
+        // 동적 쿼리
+        BooleanBuilder builder = new BooleanBuilder();
+        if("GTE".equals(opt)) {
+            builder.and(fruit.price.goe(price));
+        }
+        else {
+            builder.and(fruit.price.loe(price));
         }
         return queryFactory
                 .selectFrom(fruit)
-                .where(fruit.price.loe(price), fruit.isSold.eq(false))
+                .where(builder, fruit.isSold.eq(false))
                 .fetch()
-                .stream()
-                .map(FruitResponse::new)
-                .collect(Collectors.toList());
+                .stream().map(FruitResponse::new).collect(Collectors.toList());
     }
-
 }
