b7b2d1cf4d0b468e71b5eb462ef8db3794350caa
jooda00 <dragon2000a@gmail.com>
2024-02-28 21:19:40 +0900

    fix: FruitResponse -> Fruit 반환하도록 FruitRepository 메소드 변경

--- src/main/java/com/inflearn/Inflearn/Study/day07/service/FruitService.java
+++ src/main/java/com/inflearn/Inflearn/Study/day07/service/FruitService.java
@@ -3,21 +3,25 @@ package com.inflearn.Inflearn.Study.day07.service;
 import com.inflearn.Inflearn.Study.day07.dto.*;
 import com.inflearn.Inflearn.Study.day07.entity.Fruit;
 import com.inflearn.Inflearn.Study.day07.repository.FruitRepository;
+import com.inflearn.Inflearn.Study.day07.repository.FruitRepositoryUsingQuerydsl;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service // 스프링 빈으로 만들어 줌
 @Transactional(readOnly = true) // default값이 false
 public class FruitService {
 
     private final FruitRepository fruitRepository;
+    private final FruitRepositoryUsingQuerydsl fruitRepositoryUsingQuerydsl;
 
-    public FruitService(FruitRepository fruitRepository) {
+    public FruitService(FruitRepository fruitRepository, FruitRepositoryUsingQuerydsl fruitRepositoryUsingQuerydsl) {
         this.fruitRepository = fruitRepository;
+        this.fruitRepositoryUsingQuerydsl = fruitRepositoryUsingQuerydsl;
     }
 
     @Transactional
@@ -34,11 +38,11 @@ public class FruitService {
     }
 
     public FruitSoldResponse getFruitIsSoldOrNot(String name) {
-        long startTime = System.currentTimeMillis();
         List<Fruit> fruit = fruitRepository.findByName(name);
         if(fruit.size() == 0) {
             throw new IllegalArgumentException("이름과 일치하는 과일이 없습니다.");
         }
+        long startTime = System.currentTimeMillis();
         List<Object[]> fruits = fruitRepository.findIsSoldAndPriceByName(name);
         Long isSold = 0L;
         Long isNotSold = 0L;
@@ -60,10 +64,16 @@ public class FruitService {
     }
 
     public List<FruitResponse> getFruitGreaterThanEqualByPrice(Long price) {
-        return fruitRepository.findAllByPriceGreaterThanEqualAndIsSoldFalse(price);
+        return fruitRepository.findAllByPriceGreaterThanEqualAndIsSoldFalse(price)
+                .stream()
+                .map(FruitResponse::new)
+                .collect(Collectors.toList());
     }
 
     public List<FruitResponse> getFruitLessThanEqualByPrice(Long price) {
-        return fruitRepository.findAllByPriceLessThanEqualAndIsSoldFalse(price);
+        return fruitRepository.findAllByPriceLessThanEqualAndIsSoldFalse(price)
+                .stream()
+                .map(FruitResponse::new)
+                .collect(Collectors.toList());
     }
 }
